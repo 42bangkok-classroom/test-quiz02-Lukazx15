d9e037e133e93560aaa5e1b0d412e220d9b1b5a6
Lukazx15 <nattananpunpetch@gmail.com>
2026-01-16 19:23:43 +0700

    4

--- src/practical/p01/index.ts
+++ src/practical/p01/index.ts
@@ -1,16 +1,47 @@
 import axios from "axios";
 
-export const getPostalAddress = async (): Promise<[]> => {
-  try {
-    const res = await axios.get('https://jsonplaceholder.typicode.com/users');
-    const data = res.data;
 
-    if (!data || !Array.isArray(data) || data.length === 0) {
+interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+}
+
+interface Geo {
+  lat: string;
+  lng: string;
+}
+
+interface apiData {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address?: Address;
+  phone: string;
+  website: string;
+  company: object;
+}
+
+interface UserOutput {
+  id: number;
+  name: string;
+  phone: string;
+  address: Address | null;
+}
+export const fetchUsers = async (): Promise<UserOutput[]> => {
+  try {
+    const res = await axios.get<apiData[]>("https://jsonplaceholder.typicode.com/users");
+    const users: UserOutput[] = res.data.map((user) => ({
+      id: user.id,
+      name: user.name,
+      phone: user.phone,
+      address: user.address ? user.address : null,
+    }));
+      return users;
+    } catch (error) {
       return [];
     }
-
-    return res.data;
-  } catch (error) {
-    return [];
-  }
-};
+  };
