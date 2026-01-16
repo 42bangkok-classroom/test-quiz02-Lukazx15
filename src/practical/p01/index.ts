1e60f95b381fa369b246ae33c837ccecfdb3bb14
Lukazx15 <nattananpunpetch@gmail.com>
2026-01-16 19:19:07 +0700

    3

--- src/practical/p01/index.ts
+++ src/practical/p01/index.ts
@@ -1,17 +1,16 @@
 import axios from "axios";
 
-export const getPostalAddress = async () => {
-    try {
-        {
-            const res = await axios.get("https://jsonplaceholder.typicode.com/users");
-            const data = res.data;
-            if (!data || !Array.isArray(data) || res.data.length === 0){
-                return [];
-            }
-            return res.data;
-        }
-    } catch (error) {
-        return[];
-    }
-}
+export const getPostalAddress = async (): Promise<[]> => {
+  try {
+    const res = await axios.get('https://jsonplaceholder.typicode.com/users');
+    const data = res.data;
 
+    if (!data || !Array.isArray(data) || data.length === 0) {
+      return [];
+    }
+
+    return res.data;
+  } catch (error) {
+    return [];
+  }
+};
